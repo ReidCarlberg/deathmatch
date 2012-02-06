9ea4a5acde0196e7f55749c034e27850a98a9728
Reid Carlberg <reid.carlberg@gmail.com>
2012-02-06 16:47:36 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deathmatch::Application.config.secret_token = '3baaba2dd00b8efc63c1575d2ee5fede7a4e3ddb8d4da32224346075d99519afadddbab8fed05f4a9dfed8673e02614f01c9d7dea4715e518a7d7f8d8fbe2d55'
